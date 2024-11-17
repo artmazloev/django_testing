70da79682dbb98db03075235bfa5400ca8b7ba67
artmazloev <109161578+artmazloev@users.noreply.github.com>
2024-11-17 10:27:16 +0300

    Update test_routes.py

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -16,7 +16,7 @@ class TestRoutes(BaseTestNote):
         super().setUpTestData()
         cls.URL_LOGIN = reverse('users:login')
         cls.URL_LOGOUT = reverse('users:logout')
-        cls.URL_SINGUP = reverse('users:signup')
+        cls.URL_SIGNUP = reverse('users:signup')
         cls.URL_HOME = reverse('notes:home')
         cls.URL_LIST = reverse('notes:list')
         cls.URL_ADD = reverse('notes:add')
@@ -24,54 +24,27 @@ class TestRoutes(BaseTestNote):
         cls.URL_DETAIL = reverse('notes:detail', args=(cls.note.slug,))
         cls.URL_EDIT = reverse('notes:edit', args=(cls.note.slug,))
         cls.URL_DELETE = reverse('notes:delete', args=(cls.note.slug,))
-#Комментарий ревьюера: такой же коммент про кол-во
-
-    def test_home_availability_for_anonymous_user(self):
-        """Анонимному пользователю доступна главная страница."""
-        response = self.client.get(self.URL_HOME)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        
 
     def test_pages_availability_for_anonymous_user(self):
-        """Доступ для анонимных пользователей."""
-        urls = (
-            self.URL_HOME,
-            self.URL_LOGIN,
-            self.URL_LOGOUT,
-            self.URL_SINGUP,
-        )
-        for url in urls:
-            with self.subTest(name=url):
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_pages_availability_for_auth_user(self):
-        """Доступ страниц для авторизованного пользователя."""
-        urls = (
-            self.URL_LIST,
-            self.URL_ADD,
-            self.URL_SUCCESS
-        )
-        for url in urls:
-            with self.subTest(name=url):
-                response = self.reader_client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_pages_availability_for_different_users(self):
-        """Доступ разных страниц заметки для разных пользователей."""
-        urls = (
-            self.URL_DETAIL,
-            self.URL_EDIT,
-            self.URL_DELETE
-        )
-        clients_status = (
-            (self.reader_client, HTTPStatus.NOT_FOUND),
-            (self.author_client, HTTPStatus.OK)
-        )
-        for url in urls:
-            for client, status in clients_status:
-                with self.subTest(name=url):
-                    response = client.get(url)
-                    self.assertEqual(response.status_code, status)
+        params = (
+            (self.URL_HOME, self.client, HTTPStatus.OK),
+            (self.URL_LOGIN, self.client, HTTPStatus.OK),
+            (self.URL_LOGOUT, self.client, HTTPStatus.OK),
+            (self.URL_SIGNUP, self.client, HTTPStatus.OK),
+            (self.URL_LIST, self.reader_client, HTTPStatus.OK),
+            (self.URL_ADD, self.reader_client, HTTPStatus.OK),
+            (self.URL_SUCCESS, self.reader_client, HTTPStatus.OK),
+            (self.URL_DETAIL, self.reader_client, HTTPStatus.NOT_FOUND),
+            (self.URL_EDIT, self.reader_client, HTTPStatus.NOT_FOUND),
+            (self.URL_DELETE, self.reader_client, HTTPStatus.NOT_FOUND),
+            (self.URL_DETAIL, self.author_client, HTTPStatus.OK),
+            (self.URL_EDIT, self.author_client, HTTPStatus.OK),
+            (self.URL_DELETE, self.author_client, HTTPStatus.OK))
+        for param in params:
+            with self.subTest(name=param):
+                response = param[1].get(param[0])
+                self.assertEqual(response.status_code, param[2])
 
     def test_redirects(self):
         """Тесты редиректов"""
