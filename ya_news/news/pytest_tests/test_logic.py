3aa70f05b37ed82356438f6695143e6261ee6b1b
artmazloev <109161578+artmazloev@users.noreply.github.com>
2024-11-17 10:22:49 +0300

    Update test_logic.py

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -1,5 +1,4 @@
 from http import HTTPStatus
-
 from pytest_django.asserts import assertRedirects, assertFormError
 
 from news.forms import BAD_WORDS, WARNING
@@ -16,19 +15,18 @@ NEW_COMMENT_TEXT = 'Обновлённый комментарий'
 
 def test_anonymous_user_cant_create_comment(client, url_detail):
     """Анонимный пользователь не может создать коммент."""
+    comments_cout_before = Comment.objects.count()
     client.post(url_detail, data=COMMENT_FORM_DATA)
     comments_count = Comment.objects.count()
-    assert comments_count == 0 #Комментарий ревьюера:#тут писал про сравнивание не с конкретным числом, а с числом комментов в БД на начало теста, см коммент из прошлого ревью
-                               #это нужно поправить везде
-                               #то есть находишь кол-во сущностей в БД на начало теста, выполняешь действия и в конце заново находишь записи в БД и смотришь как изменилось или не изменилось это число
+    assert comments_count == comments_cout_before
 
 
 def test_user_can_create_comment(new, author_client, author, url_detail):
     """Авторизованный пользователь может создать коммент."""
-    response = author_client.post(url_detail, data=COMMENT_FORM_DATA)
-    assertRedirects(response, f'{url_detail}#comments') #Комментарий ревьюера: а у нас же для этого отдельный тест есть
+    comments_count_before = Comment.objects.count()
+    author_client.post(url_detail, data=COMMENT_FORM_DATA)
     comments_count = Comment.objects.count()
-    assert comments_count == 1 #Комментарий ревьюера: коммент с подсчетом поправить везде
+    assert comments_count == comments_count_before + 1
     comment = Comment.objects.get()
     assert comment.text == COMMENT_TEXT
     assert comment.news == new
@@ -37,6 +35,7 @@ def test_user_can_create_comment(new, author_client, author, url_detail):
 
 def test_user_cant_use_bad_words(author_client, url_detail):
     """Автор - культурный человек."""
+    comments_count_before = Comment.objects.count()
     response = author_client.post(url_detail, data=BAD_WORDS_DATA)
     assertFormError(
         response,
@@ -45,11 +44,12 @@ def test_user_cant_use_bad_words(author_client, url_detail):
         errors=WARNING
     )
     comments_count = Comment.objects.count()
-    assert comments_count == 0
+    assert comments_count == comments_count_before
 
 
 def test_author_can_delete_comment(author_client, url_delete, url_detail):
     """Автор может удалять коммент."""
+    comments_count = Comment.objects.count()
     response = author_client.delete(url_delete)
     assertRedirects(response, url_detail + '#comments')
     comments_count = Comment.objects.count()
@@ -58,10 +58,11 @@ def test_author_can_delete_comment(author_client, url_delete, url_detail):
 
 def test_not_author_cant_delete_comment(not_author_client, url_delete):
     """Не автор не может удалять коммент."""
+    comments_count_before = Comment.objects.count()
     response = not_author_client.delete(url_delete)
     assert response.status_code == HTTPStatus.NOT_FOUND
     comments_count = Comment.objects.count()
-    assert comments_count == 1
+    assert comments_count == comments_count_before
 
 
 def test_author_can_edit_comment(author_client, comment, url_edit, url_detail):
